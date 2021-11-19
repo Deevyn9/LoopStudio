f56b42d1c2d19824044930703cfae3aa8c570213
Divine Obeten <deevynobeeten@gmail.com>
2021-11-19 21:23:22 +0100

    Updating responsiveness and navbar positioning

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ const toggleButton = document.getElementsByClassName("toggle-button")[0]
 const navbarLinks = document.getElementsByClassName("navbar-links")[0]
 const navbar = document.querySelector(".navbar");
 
-
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
     document.body.classList.toggle("cross");
     document.body.classList.toggle("black");    
+});
+
+window.addEventListener('scroll',  function() {
+    if (window.scrollY > 780) {
+        navbar.style.position = 'fixed';
+        navbar.style.backgroundColor = 'black'; 
+    }else{
+        navbar.style.position = 'absolute';
+        navbar.style.backgroundColor = "var(--nav-bg)";
+    }
 });
